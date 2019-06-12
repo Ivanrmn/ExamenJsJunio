2c4aaebe7cc73f34c5d26aaa2cac6a79e2d77424
ivanrmn <ivanrmnh@gmail.com>
2019-06-12 20:02:58 +0200

    fastBreakTruck, create smiggles and eyeholes

---
+++ fastBreakTruck.js
@@ -0,0 +1,35 @@
+var requireDesayuno = require('./desayuno');
+const protoDesayuno = requireDesayuno.singletonDesayuno().get();
+var requireItem = require('./item');
+const protoItem = requireItem.singletonItem().get();
+
+class FastBreakTruck {
+	constructor() {
+	}
+	
+}
+
+var singletonDesayuno = require('./desayuno');
+
+FastBreakTruck.prototype.prepararEyeHoles = function() {
+    this.prepararEyeHoles = singletonDesayuno.singletonDesayuno().get();
+    return Object.create(this.prepararEyeHoles);
+};
+
+FastBreakTruck.prototype.prepararSmiggles = function() {
+    this.prepararSmiggles = singletonDesayuno.singletonDesayuno().get();
+    return Object.create(this.prepararSmiggles);
+};
+
+function singletonFastBreakTruck() {
+	var fastBreakTruck = new FastBreakTruck();
+	return {
+		get: function() {
+			return fastBreakTruck;
+		}
+	};
+}
+
+exports.singletonFastBreakTruck = function() {
+	return singletonFastBreakTruck();
+};
